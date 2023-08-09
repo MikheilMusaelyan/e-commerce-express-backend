2252420c588a1c6a9a51cb0b03143d59a3e9f398
MikheilMusaelyan <mishkamusaeliani@gmail.com>
2023-08-09 15:17:26 -0400

    setup the server

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const http = require("http");
+
+require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || '3000'
+
+const server = http.createServer(app);
+app.set('port', port);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', "*");
@@ -22,41 +36,8 @@ app.use((req, res, next) => {
 });
 
 app.post('/create-customer', async (req, res) => {
-    // Create a new customer object
-    const customer = await stripe.customers.create({
-        email: req.body.email,
-    });
-    // save the customer.id as stripeCustomerId
-    res.send({ customer });
-});
+   
+}); // can I put post here? and app.use?
 
-app.post('/create-subscription', async (req, res) => { // Set the default payment method on the customerlet paymentMethod;
-    try {
-        paymentMethod = await stripe.paymentMethods.attach(
-            req.body.paymentMethodId.id,
-            { customer: req.body.customerID }
-        );
-    } catch (error) {
-        return res.status(200).send({ 
-            error: { message: error.message } 
-        });
-    }
-    
-    let updateCustomerDefaultPaymentMethod = await stripe.customers.update( 
-        req.body.customerID,
-        { 
-            invoice_settings: { 
-                default_payment_method: paymentMethod.id 
-            } 
-        }
-    );
-
-    subscription = await stripe.subscriptions.create({
-      customer: req.body.customerID,
-      items: [{ price: "p" }],
-      expand: ["latest_invoice.payment_intent"]
-    })
-    res.send(subscription)
-}); 
 
 module.exports = app
