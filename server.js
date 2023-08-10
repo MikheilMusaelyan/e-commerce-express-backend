8c6319cc799d7ce1c6d684d08ba524412e2824ac
MikheilMusaelyan <mishkamusaeliani@gmail.com>
2023-08-10 18:10:01 -0400

    add another paying method

--- server.js
+++ server.js
@@ -44,33 +44,67 @@ app.post('/create-customer', async (req, res) => {
     res.send({ customer });
 });
 
-app.post('/create-subscription', async (req, res) => { // Set the default payment method on the customerlet paymentMethod;
-    try {
-        paymentMethod = await stripe.paymentMethods.attach(
-            req.body.paymentMethodId.id,
-            { customer: req.body.customerID }
-        );
-    } catch (error) {
-        return res.status(200).send({ 
-            error: { message: error.message } 
-        });
-    }
+// app.post('/create-subscription', async (req, res) => { // Set the default payment method on the customerlet paymentMethod;
+//     try {
+//         paymentMethod = await stripe.paymentMethods.attach(
+//             req.body.paymentMethodId.id,
+//             { customer: req.body.customerID }
+//         );
+//     } catch (error) {
+//         return res.status(200).send({ 
+//             error: { message: error.message } 
+//         });
+//     }
     
-    let updateCustomerDefaultPaymentMethod = await stripe.customers.update( 
-        req.body.customerID,
-        { 
-            invoice_settings: { 
-                default_payment_method: paymentMethod.id 
-            } 
-        }
-    );
+//     let updateCustomerDefaultPaymentMethod = await stripe.customers.update( 
+//         req.body.customerID,
+//         { 
+//             invoice_settings: { 
+//                 default_payment_method: paymentMethod.id 
+//             } 
+//         }
+//     );
 
-    subscription = await stripe.subscriptions.create({
-      customer: req.body.customerID,
-      items: [{ price: process.env.PRICE_ID }],
-      expand: ["latest_invoice.payment_intent"]
-    })
-    res.send(subscription)
-}); 
+//     subscription = await stripe.subscriptions.create({
+//       customer: req.body.customerID,
+//       items: [{ price: process.env.PRICE_ID }],
+//       expand: ["latest_invoice.payment_intent"]
+//     })
+//     res.send(subscription)
+// }); 
+
+app.post('/create-checkout-session', async (req, res) => {
+  const { priceId } = req.body;
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: 'subscription',
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price: priceId,
+          // For metered billing, do not pass quantity
+          quantity: 1,
+        },
+      ],
+      // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
+      // the actual Session ID is returned in the query parameter when your customer
+      // is redirected to the success page.
+      success_url: 'https://example.com/success',
+      cancel_url: 'https://example.com/cancel',
+    });
+
+    res.send({
+      sessionId: session.id,
+    });
+  } catch (e) {
+    res.status(400);
+    return res.send({
+      error: {
+        message: e.message,
+      }
+    });
+  }
+});
 
 module.exports = app
